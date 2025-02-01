0ad6f66279f1cdcd4e9ae07e048d62e715057b93
Erwin Janssen <erwinjanssen@outlook.com>
2025-02-01 03:00:10 +0100

    Initialize repository as Nix Flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment personal website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+        ];
+      };
+    };
+}
